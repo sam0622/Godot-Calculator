2e4048507bbecd6d64d95cba2a607740736e7b96
sam0622 <sgreenfield517@gmail.com>
2023-11-29 13:04:23 -0500

    code everything and make it work

--- NumberButtons.gd
+++ NumberButtons.gd
@@ -1,11 +1,40 @@
 extends Control
-signal button_pressed(string)
+signal button_pressed()
+signal operation_pressed()
+
+func _on_button_pressed(number):# Handles number signals
+	print(number)
+	match number:
+		1:
+			button_pressed.emit(1)
+		2:
+			button_pressed.emit(2)
+		3:
+			button_pressed.emit(3)
+		4:
+			button_pressed.emit(4)
+		5:
+			button_pressed.emit(5)
+		6:
+			button_pressed.emit(6)
+		7:
+			button_pressed.emit(7)
+		8:
+			button_pressed.emit(8)
+		9:
+			button_pressed.emit(9)
+		0: 
+			button_pressed.emit(0)
 
 
-
-func _on_button_pressed(extra_arg_0):
-	match extra_arg_0:
-		case "1":
-			button_pressed.emit("1")
-		case "2": button_pressed.emit("2")
+func _on_operation_button_pressed(operation):# Handles operation signals
+	match operation:
+		"addition":
+			operation_pressed.emit("+")
+		"subtraction":
+			operation_pressed.emit("-")
+		"multiplication":
+			operation_pressed.emit("*")
+		"division":
+			operation_pressed.emit("/")
 			
--- calculator.gd
+++ calculator.gd
@@ -5,8 +5,7 @@ var result = null
 var operation = null
 
 
-
-func _on_calculate_button_pressed():
+func _on_calculate_button_pressed():# Preforms operations on inputs
 	if operation == "+":
 		result = input_1 + input_2
 	elif operation == "-":
@@ -14,10 +13,25 @@ func _on_calculate_button_pressed():
 	elif operation == "*":
 		result = input_1 * input_2
 	elif operation == "/":
-		result = input_1 / input_2
+		result = float(input_1) / float(input_2)
 	$Result.text = str(result)
 	input_1 = null
 	input_2 = null
 	operation = null
-	
 
+
+func _on_number_buttons_button_pressed(number):# Handles changing and displaying equation
+	if operation == null:
+		input_1 = number
+		$Input.text = ""
+		$Input.text += str(number)
+	else:
+		input_2 = number
+		$Input.text += str(number)
+
+
+
+
+func _on_calculator_buttons_operation_pressed(string):
+	$Input.text += string
+	operation = string
